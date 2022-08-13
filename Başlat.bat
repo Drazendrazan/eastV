78a188cd472b1d3a83b8fb58931b08e063dfc9fd
dreamteamdev <hayaletarma3@gmail.com>
2022-08-13 04:34:25 +0300

    Update Başlat.bat

--- Başlat.bat
+++ Başlat.bat
@@ -1,3 +1,3 @@
-﻿cd /d E:\eastV1.0
-start E:\eastV1.0\Artifacts\FXServer.exe +exec E:\eastV1.0\server.cfg +set sv_enforceGameBuild 2545 +set svgui_disable true +set onesync on +set onesync_enableInfinity 1
+﻿cd /d C:\Users\user\Documents\GitHub\eastV
+start C:\Users\user\Documents\GitHub\eastV\Artifacts\FXServer.exe +exec C:\Users\user\Documents\GitHub\eastV\server.cfg +set sv_enforceGameBuild 2545 +set svgui_disable true +set onesync on +set onesync_enableInfinity 1
 exit
