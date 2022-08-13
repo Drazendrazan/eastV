06aa4e6d54915dff0d6ea33f0ad86ed2f58c654e
dreamteamdev <hayaletarma3@gmail.com>
2022-08-13 10:24:44 +0300

    printer

---
+++ resources/[QBCore-Scripts]/qb-printer/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+ui_page "html/index.html"
+
+client_scripts {
+    'client/main.lua',
+}
+
+server_scripts {
+    'server/main.lua',
+}
+
+files {
+    '*.lua',
+    'html/*.html',
+    'html/*.js',
+    'html/*.css',
+    'html/*.png',
+}
