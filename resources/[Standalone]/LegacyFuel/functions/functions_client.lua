3e3cf6918cbff4cc635481bd13ff9bd3742a453b
dreamteamdev <hayaletarma3@gmail.com>
2022-08-13 12:38:34 +0300

    fuel nozzle

--- resources/[Standalone]/LegacyFuel/functions/functions_client.lua
+++ resources/[Standalone]/LegacyFuel/functions/functions_client.lua
@@ -14,9 +14,31 @@ function LoadAnimDict(dict)
 		RequestAnimDict(dict)
 
 		while not HasAnimDictLoaded(dict) do
-			Wait(1)
+			Citizen.Wait(1)
 		end
 	end
+end
+
+function Round(num, numDecimalPlaces)
+	local mult = 10^(numDecimalPlaces or 0)
+
+	return math.floor(num * mult + 0.5) / mult
+end
+
+function CreateBlip(coords)
+	local blip = AddBlipForCoord(coords)
+
+	SetBlipSprite(blip, 361)
+	SetBlipScale(blip, 0.9)
+	SetBlipColour(blip, 4)
+	SetBlipDisplay(blip, 4)
+	SetBlipAsShortRange(blip, true)
+
+	BeginTextCommandSetBlipName("STRING")
+	AddTextComponentString("Gas Station")
+	EndTextCommandSetBlipName(blip)
+
+	return blip
 end
 
 function DrawText3Ds(x, y, z, text)
@@ -34,55 +56,8 @@ function DrawText3Ds(x, y, z, text)
 	end
 end
 
-function Round(num, numDecimalPlaces)
-	local mult = 10^(numDecimalPlaces or 0)
-
-	return math.floor(num * mult + 0.5) / mult
-end
-
-function CreateBlip(coords)
-	local blip = AddBlipForCoord(coords)
-
-	SetBlipSprite(blip, 361)
-	SetBlipScale(blip, 0.6)
-	SetBlipColour(blip, 4)
-	SetBlipDisplay(blip, 4)
-	SetBlipAsShortRange(blip, true)
-
-	BeginTextCommandSetBlipName("STRING")
-	AddTextComponentString("Gas Station")
-	EndTextCommandSetBlipName(blip)
-
-	return blip
-end
-
-function FindNearestFuelPump()
-	local coords = GetEntityCoords(PlayerPedId())
-	local fuelPumps = {}
-	local handle, object = FindFirstObject()
-	local success
-
-	repeat
-		if Config.PumpModels[GetEntityModel(object)] then
-			table.insert(fuelPumps, object)
-		end
-
-		success, object = FindNextObject(handle, object)
-	until not success
-
-	EndFindObject(handle)
-
-	local pumpObject = 0
-	local pumpDistance = 1000
-
-	for _, fuelPumpObject in pairs(fuelPumps) do
-		local dstcheck = #(coords - GetEntityCoords(fuelPumpObject))
-
-		if dstcheck < pumpDistance then
-			pumpDistance = dstcheck
-			pumpObject = fuelPumpObject
-		end
-	end
-
-	return pumpObject, pumpDistance
+function ShowNotification(msg)
+	SetNotificationTextEntry('STRING')
+	AddTextComponentString(msg)
+	DrawNotification(0,1)
 end
