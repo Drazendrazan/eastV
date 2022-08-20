65160dea75da2ce51601f5ae9df39eba5cce1770
dreamteamdev <hayaletarma3@gmail.com>
2022-08-20 15:26:46 +0300

    Update en.lua

--- resources/[Standalone]/ps-hud/locales/en.lua
+++ resources/[Standalone]/ps-hud/locales/en.lua
@@ -1,21 +1,21 @@
 local Translations = {
     notify = {
-        ["hud_settings_loaded"] = "HUD Settings Loaded!",
-        ["hud_restart"] = "HUD Is Restarting!",
-        ["hud_start"] = "HUD Is Now Started!",
-        ["hud_command_info"] = "This command resets your current HUD settings!",
-        ["load_square_map"] = "Square Map Loading...",
-        ["loaded_square_map"] = "Square Map Has Loaded!",
-        ["load_circle_map"] = "Circle Map Loading...",
-        ["loaded_circle_map"] = "Circle Map Has Loaded!",
-        ["cinematic_on"] = "Cinematic Mode On!",
-        ["cinematic_off"] = "Cinematic Mode Off!",
-        ["engine_on"] = "Engine Started!",
-        ["engine_off"] = "Engine Shut Down!",
-        ["low_fuel"] = "Fuel Level Low!",
-        ["access_denied"] = "You Are Not Authorized!",
-        ["stress_gain"] = "Feeling More Stressed!",
-        ["stress_removed"] = "Feeling More Relaxed!"
+        ["hud_settings_loaded"] = "HUD Ayarları Yükleniyor!",
+        ["hud_restart"] = "HUD Tekrar Başlatılıyor!",
+        ["hud_start"] = "HUD Başlatıldı!",
+        ["hud_command_info"] = "Bu komut hud ayarlarını sıfırlar!",
+        ["load_square_map"] = "Harita Yükleniyor...",
+        ["loaded_square_map"] = "Harita Yüklendi!",
+        ["load_circle_map"] = "Circle Harita Yükleniyor...",
+        ["loaded_circle_map"] = "Circle Harita Yüklendi!!",
+        ["cinematic_on"] = "Cinematic Mode Açık!",
+        ["cinematic_off"] = "Cinematic Mode Kapalı!",
+        ["engine_on"] = "Motor Açıldı!",
+        ["engine_off"] = "Motor Kapatıldı!",
+        ["low_fuel"] = "Benzinin Bitmek Üzere!",
+        ["access_denied"] = "Yetkili Değilsin!",
+        ["stress_gain"] = "Stresleniyorsun!",
+        ["stress_removed"] = "Sakinleşiyorsun!"
     }
 }
 Lang = Locale:new({phrases = Translations, warnOnMissing = true})
