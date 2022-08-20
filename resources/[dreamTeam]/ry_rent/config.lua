6964de6a27f6a6211da863c627454c6a9efaa900
dreamteamdev <hayaletarma3@gmail.com>
2022-08-20 12:31:17 +0300

    Update config.lua

--- resources/[dreamTeam]/ry_rent/config.lua
+++ resources/[dreamTeam]/ry_rent/config.lua
@@ -6,12 +6,12 @@ Config.Options = {
     ['delete_vehicle'] = true, -- delete vehicle when rent time is finished and not delivered? Default true
     ['delete_time'] = 25000, -- time in MS that vehicle will be deleted if delete function is active and after time is finished. Default 25000MS | 25Seconds. 
 
-    ['time_finished'] = 'Your rental time is up, thank you.',
-    ['spawnpoint_blocked'] = 'Another vehicle is taking the spawn place.',  -- message when spawn_point is blocked by other vehicle.
-    ['no_money'] = 'You dont have enought money to rent the vehicle.', -- message when don't have money.
-    ['return_success'] = 'Successfully returned the vehicle, thank you!', -- message when return the vehicle is Successfully.
-    ['return_error'] = 'You need to be in the vehicle you rented.', -- message when the player its not in rented vehicle.
-    ['cant_rent'] = 'You already rented a vehicle', -- message when the player already have a rented vehicle.
+    ['time_finished'] = 'Kiralama süreniz doldu, teşekkürler.',
+    ['spawnpoint_blocked'] = 'Başka bir araç garaj çıkışında.',  -- message when spawn_point is blocked by other vehicle.
+    ['no_money'] = 'Aracı kiralamak için yeterli paranız yok.', -- message when don't have money.
+    ['return_success'] = 'Aracı başarıyla iade ettin, teşekkürler!', -- message when return the vehicle is Successfully.
+    ['return_error'] = 'Kiraladığınız araçta olmanız gerekmektedir.', -- message when the player its not in rented vehicle.
+    ['cant_rent'] = 'Zaten bir araç kiraladınız', -- message when the player already have a rented vehicle.
 }
 
 Config.Locations = {
@@ -25,8 +25,8 @@ Config.Locations = {
                 type = 2, -- type of marker.
                 size  = {x = 0.3, y = 0.3, z = 0.3},  -- size of marker.
                 color = {r = 255, g = 255, b = 255},  -- color of marker.
-                text = '[ ~g~E~w~ ] Rent Vehicle', -- text of marker.
-                text = '[ ~g~E~w~ ] Rent Vehicle' -- text of marker.
+                text = '[ ~g~E~w~ ] Araç Kirala', -- text of marker.
+                text = '[ ~g~E~w~ ] Araç Kirala' -- text of marker.
             },
             return_spot = {
                 key = 47, -- key to open the menu. Default G
@@ -38,13 +38,13 @@ Config.Locations = {
         },
         blips = {
             spawn = {
-                name = 'Rent Vehicle', -- name of the blip in map.
+                name = 'Araç Kirala', -- name of the blip in map.
                 sprite = 523, -- sprite of the blip.
                 scale = 0.8, -- scale of the.
                 color = 2 -- color of the. 
             },
             return_spot = {
-                name = 'Return rented Vehicle', -- name of the return blip in map.
+                name = 'Aracı teslim et', -- name of the return blip in map.
                 sprite = 523, -- sprite of the return blip.
                 scale = 0.8, -- scale of the return blip.
                 color = 4 -- color of the return blip. 
